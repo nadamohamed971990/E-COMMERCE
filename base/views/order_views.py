e07496cb582594c4234502831d16ecdbc7141051
NadaMohamed90 <107099709+nadamohamed971990@users.noreply.github.com>
2022-08-28 01:47:03 +0200

    Add files via upload

---
+++ base/views/order_views.py
@@ -0,0 +1,146 @@
+# Django Import
+from email import message
+from email.mime import base
+from multiprocessing import context
+from re import template
+from unicodedata import name
+from django.core.exceptions import RequestDataTooBig
+from django.shortcuts import render
+from datetime import datetime
+
+from rest_framework import status
+
+
+from rest_framework.response import Response
+
+# Rest Framework Import
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from backend.settings import EMAIL_HOST_USER
+
+
+# Local Import
+from base.products import products
+from base.models import *
+from base.serializers import ProductSerializer, OrderSerializer
+
+# checkout
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+
+from django.core.mail import send_mail
+
+
+# views start from here
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+    print(data)
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No Order Items', "status": status.HTTP_400_BAD_REQUEST})
+    else:
+        # (1) Create Order
+        order = Order.objects.create(
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice'],
+        )
+
+        # (2) Create Shipping Address
+
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
+        )
+
+        # (3) Create order items
+
+        for i in orderItems:
+            product = Product.objects.get(_id=i['product'])
+
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url,
+            )
+
+            # (4) Update Stock
+
+            product.countInStock -= item.qty
+            product.save()
+
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getMyOrders(request):
+    user = request.user
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request, pk):
+
+    user = request.user
+
+    try:
+        order = Order.objects.get(_id=pk)
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            Response({'detail': 'Not Authorized  to view this order'},
+                     status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request,pk):
+    order = Order.objects.get(_id=pk)
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+    return Response('Order was paid')
+
+    
+
+ 
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateOrderToDelivered(request, pk):
+    order = Order.objects.get(_id=pk)
+    order.isDeliver = True
+    order.deliveredAt = datetime.now()
+    order.save()
+    return Response('Order was Delivered')
