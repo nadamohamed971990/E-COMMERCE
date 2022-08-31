ce525a0693ba33de74ca95f8ef9a8d1624e083a9
nada <nada.mohamed.ahmed9790@gmail.com>
2022-08-31 18:08:20 +0200

    make it better

--- backend/settings.py
+++ backend/settings.py
@@ -186,7 +186,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR ,"media")
-STATICFILES_STORAGE ='whitenose.django.GzipmanifestStaticFilesStorage'
+#STATICFILES_STORAGE ='whitenose.django.GzipmanifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     BASE_DIR/'static',
---
+++ base/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+admin.site.register(AddImg)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "user","createdAt","totalPrice"
+    ]
