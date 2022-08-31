7d799dfca686a25f4b527fd79d9cc7b53d41b638
nada <nada.mohamed.ahmed9790@gmail.com>
2022-08-31 19:25:12 +0200

    make it better

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,112 @@
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+/* REACT BOOTSTRAP */
+import { Navbar, Nav, Container, NavDropdown, Card } from "react-bootstrap";
+
+/* REACT ROUTER BOOTSTRAP */
+import { LinkContainer } from "react-router-bootstrap";
+
+/* REACT - REDUX */
+import { useDispatch, useSelector } from "react-redux";
+
+/* ACTION CREATORS */
+import { logout } from "../actions/userActions";
+
+/* COMPONENTS */
+import SearchBox from "./SearchBox";
+
+import logo from "../logo.png";
+
+function Header(userImg) {
+  /* PULLING A PART OF STATE FROM THE ACTUAL STATE IN THE REDUX STORE */
+  const userLogin = useSelector((state) => state.userLogin);
+
+  const { userInfo } = userLogin;
+
+  /* HANDLER */
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="Ecommerce" className="logo" />
+            </Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls="navbarScroll" />
+
+          <Navbar.Collapse id="navbarScroll">
+            <SearchBox />
+
+            <Nav
+              className="ms-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/admin/imglist">
+                    <Link to={`/get_user/${userImg._id}`}>
+                      <Card.Img src={userImg.image} />
+                    </Link>
+                  </LinkContainer>
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i> Login
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <LinkContainer to="/admin/imglist">
+                    <Link to={`/get_user/${userImg._id}`}>
+                      <Card.Img src={userImg.image} />
+                    </Link>
+                  </LinkContainer>
+
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
---
+++ frontend/src/userImg.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+const userImg = [
+  {
+    _id: "1",
+    image: "/images/airpods.jpg",
+  },  
+  {
+    _id: "2",
+    image: "/images/phone.jpg",
+  }
+]
