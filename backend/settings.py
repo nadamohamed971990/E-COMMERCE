5adf6e86f503144b2b173375c449350b232a8f4e
nada <nada.mohamed.ahmed9790@gmail.com>
2022-08-31 20:54:16 +0200

    make it better

--- backend/settings.py
+++ backend/settings.py
@@ -187,7 +187,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR ,"media")
 
 STATICFILES_DIRS = [
     BASE_DIR/'static',
-    BASE_DIR/'/build/static', 
+    BASE_DIR/'build/static', 
     
 ]
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
